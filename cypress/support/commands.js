a5e76596f45d4c31e337717b2d873586696d588a
Murilo_Silva <murilo.franca@benner.com.br>
2025-08-04 21:17:08 -0300

    testes refatorados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,15 +5,3 @@ Cypress.Commands.add('assertLoadingIsShownAndHidden', () => {
   cy.contains('Loading ...').should('not.exist')
 })
 
-Cypress.Commands.add('assertLoadingIsShownAndHidden_fixed', (page) => {
-  cy.intercept({
-    method: 'GET',
-    pathname: '**/search',
-    query: {
-      query: 'React',
-      page: `${page}`
-    }
-  }).as('loadingPage')
-  cy.visit('/')
-  cy.wait('@loadingPage')
-})
